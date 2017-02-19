de24dfdf5a3ec850c1f09a4826c3a7459d94ec0f
Ari Ingber <ariingber@gmail.com>
2017-02-19 16:38:23 -0500

    added tap status

--- server.js
+++ server.js
@@ -12,8 +12,3 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
-
-
-app.listen(4000, function () {
-  console.log('Booyah')
-})
